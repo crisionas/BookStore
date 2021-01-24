aaa680ce38f792606e5da3bf404c500e1acc1aa9
cristian.ionas <cris.ionas@gmail.com>
2021-01-24 16:17:25 +0200

    Unit testing project init

---
+++ BookStoreTests/Controllers/BookControllerTests.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BookStore.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BookStore.Controllers.Tests
+{
+    [TestClass()]
+    public class BookControllerTests
+    {
+        [TestMethod()]
+        public void GetAllBooksTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
---
+++ BookStoreTests/Controllers/UserControllerTests.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BookStore.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BookStore.Controllers.Tests
+{
+    [TestClass()]
+    public class UserControllerTests
+    {
+        [TestMethod()]
+        public void AddBookTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
+
